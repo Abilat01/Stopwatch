69cdd6be7735f094277b281032404f61ab7135a4
Abilat01 <Abilat01@yandex.ru>
2021-07-12 20:03:07 +0300

    Шоцк, это работает, надо только исправить блювотный дизайн на чуть менее блювотный

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var stopWatchTimer = Timer()
+    var currentTime = 0
+    
     
     @IBOutlet weak var minutesLabel: UILabel!
     @IBOutlet weak var secondLabel: UILabel!
@@ -18,19 +21,57 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        pauseButton.isHidden = false
-        startButton.isHidden = true
+        pauseButton.isHidden = true
+        startButton.isHidden = false
     }
 
 
     
     @IBAction func StartButtonActivity(_ sender: UIButton) {
+        
+        startButton.isHidden = true
+        pauseButton.isHidden = false
+        stopButton.isEnabled = true
+        
+        //вот он тот самый метод который и надо разобратся как писать селектор
+        stopWatchTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(self.updateTime)), userInfo: nil, repeats: true)
+        
     }
     
     @IBAction func stopButtonActivity(_ sender: UIButton) {
+        
+        pauseButton.isHidden = true
+        startButton.isHidden = false
+        stopButton.isEnabled = false
+        
+        currentTime = 0
+        minutesLabel.text = "0"
+        secondLabel.text = "0"
+        
+        
+        stopWatchTimer.invalidate() //стоп
+        
+    }
+    
+    @objc func updateTime() {
+        
+        currentTime += 1
+        minutesLabel.text = "\(currentTime / 60)"
+        secondLabel.text = "\(currentTime % 60)"
+        
+        if currentTime == 3600 {
+            currentTime = 0
+        } //в целом логично, но сам бы я долго додумывался
+        
     }
     
     @IBAction func pauseButtonActivity(_ sender: UIButton) {
+        
+        startButton.isHidden = false
+        pauseButton.isHidden = true
+        
+        stopWatchTimer.invalidate() //пауза для таймера
+        
     }
     
 }
