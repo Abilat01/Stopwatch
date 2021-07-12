85cae85cc95dd38d8083d823ecb9c45e15924b52
Abilat01 <Abilat01@yandex.ru>
2021-07-12 19:25:49 +0300

    View

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var minutesLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pauseButton.isHidden = false
+        startButton.isHidden = true
     }
 
 
+    
+    @IBAction func StartButtonActivity(_ sender: UIButton) {
+    }
+    
+    @IBAction func stopButtonActivity(_ sender: UIButton) {
+    }
+    
+    @IBAction func pauseButtonActivity(_ sender: UIButton) {
+    }
+    
 }
 
